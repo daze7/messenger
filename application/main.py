09be263055f49f5bbaf5706dee426393b7c23338
daze <is4effvladis@yandex.ru>
2022-04-25 12:59:03 +0300

    contact list

--- application/main.py
+++ application/main.py
@@ -2,6 +2,7 @@ import datetime
 import sys
 import datetime as dt
 import sqlite3
+import webbrowser
 
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
@@ -14,6 +15,7 @@ class Autorize_Form(QDialog):
         uic.loadUi('autorize.ui', self)
         self.signin.clicked.connect(self.sign_in)
         self.signup.clicked.connect(self.sign_up)
+        self.password.setEchoMode(QLineEdit.Password)
 
     def sign_in(self):
         login = self.login.text()
@@ -22,11 +24,12 @@ class Autorize_Form(QDialog):
             con = sqlite3.connect('data/datebase/application.db')
             cur = con.cursor()
             com = 'SELECT password FROM user WHERE login = "' + login + '"'
-            res = cur.execute(com).fetchone()
+            res = cur.execute(com).fetchone()[0]
             con.close()
             if res == password:
                 self.w = Main_Window()
                 self.w.show()
+                self.close()
             else:
                 msg = QMessageBox()
                 msg.setWindowTitle("Ошибка")
@@ -41,14 +44,15 @@ class Autorize_Form(QDialog):
             msg.exec_()
 
     def sign_up(self):
-        self.w = Sign_up_form()
-        self.w.show()
+        self.q = Sign_up_form()
+        self.q.show()
 
 class Sign_up_form(QDialog):
     def __init__(self):
         super().__init__()
         uic.loadUi('sign_up_interface.ui', self)
         self.confirm.clicked.connect(self.enter)
+        self.up_password.setEchoMode(QLineEdit.Password)
 
     def enter(self):
         login = self.up_login.text()
@@ -69,14 +73,34 @@ class Sign_up_form(QDialog):
                 msg.setIcon(QMessageBox.Warning)
                 msg.exec_()
             else:
-                con = sqlite3.connect('data/datebase/application.db')
-                cur = con.cursor()
-                com = 'INSERT INTO user(id_user, login, name, surname, password, date_regist) VALUES(?, ?, ?, ?, ?, ?)'
-                user_id = 111
-                dat = [user_id, login, name, surname, password, date]
-                self.cur.execute(com, dat)
-                self.con.commit()
-                con.close()
+                status = True
+                symbols = '''qwertyuiopasdfghjklzxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM0123456789! " # $ % & ' ( ) * + ,
+                 - . / : ; < = > ? @ [ \ ] ^ _` { | } ~ '''
+                for i in password:
+                    if i not in symbols:
+                        status = False
+                        break
+                if not status:
+                    msg = QMessageBox()
+                    msg.setWindowTitle("Ошибка")
+                    msg.setText("Недопустимый пароль")
+                    msg.setIcon(QMessageBox.Warning)
+                    msg.exec_()
+                else:
+                    con = sqlite3.connect('data/datebase/application.db')
+                    cur = con.cursor()
+                    com = 'INSERT INTO user(id_user, login, name, surname, password, date_regist) VALUES(?, ?, ?, ?, ?, ?)'
+                    user_id = '111'
+                    dat = [user_id, login, name, surname, password, date]
+                    cur.execute(com, dat)
+                    con.commit()
+                    con.close()
+                    self.close()
+                    msg = QMessageBox()
+                    msg.setIcon(QMessageBox.Information)
+                    msg.setWindowTitle("Регистрация")
+                    msg.setText("Вы успешно зарегистрировались!")
+                    msg.exec_()
         else:
             msg = QMessageBox()
             msg.setWindowTitle("Ошибка")
@@ -89,11 +113,14 @@ class Sign_up_form(QDialog):
 
 
 
+
+
 class Main_Window(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main_interface.ui', self)
         self.msg_send.clicked.connect(self.send)
+        self.add_user.clicked.connect(self.search)
 
     def keyPressEvent(self, event):
         if event.key() == 16777220:
@@ -105,10 +132,19 @@ class Main_Window(QMainWindow):
             now = dt.datetime.now().strftime("%H:%M")
             con = sqlite3.connect('data/datebase/application.db')
             cur = con.cursor()
-
             con.close()
             self.msg_text.clear()
             self.msg_field.append('[' + now + '] ' + text)
+
+    def search(self):
+        self.f = Search_user()
+        self.f.show()
+
+class Search_user(QMainWindow):
+    def __int__(self):
+        super().__init__()
+        uic.loadUi('search_user2.ui', self)
+        #self.add_user.clicked.connect(self.add_to_list)
 
 
 
