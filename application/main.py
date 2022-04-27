d2cb4c96fc0ddeb49fe2f4077d35add82cadee1d
Черноусов Владимир <chernousov2005@mail.ru>
2022-04-27 16:04:01 +0300

    complete authorization and registration

--- application/main.py
+++ application/main.py
@@ -8,6 +8,7 @@ import requests
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QDialog, QMessageBox
+from werkzeug.security import check_password_hash
 
 
 class Autorize_Form(QDialog):
@@ -27,11 +28,42 @@ class Autorize_Form(QDialog):
         res = cur.execute(com).fetchall()
         cur.close()
         con.close()
-        print(res)
-        if len(res) == 0:
+        if login and password:
             try:
                 re = requests.post('http://127.0.0.1:5000/user_info').json()
-                print(re)
+                con = sqlite3.connect('data/datebase/application.db')
+                cur = con.cursor()
+                com = 'SELECT login FROM users'
+                value = ''.join([i[0] for i in cur.execute(com).fetchall()])
+                for i in re:
+                    if re[i]['login'] not in value:
+                        cur.execute(f"INSERT INTO users(user_id,login,name,surname,password,date_regist,status) "
+                                    f"VALUES('{re[i]['user_id']}','{re[i]['login']}','{re[i]['name']}',"
+                                    f"'{re[i]['surname']}', '{re[i]['password']}', '{re[i]['date_register']}', "
+                                    f"'{re[i]['status']}')")
+                        con.commit()
+                if login not in value:
+                    msg = QMessageBox()
+                    msg.setWindowTitle("Ошибка")
+                    msg.setText("Неправильный пароль")
+                    msg.setIcon(QMessageBox.Warning)
+                    msg.exec_()
+                    cur.close()
+                    con.close()
+                com = f'SELECT password FROM users WHERE login = "{login}"'
+                res = cur.execute(com).fetchone()[0]
+                if check_password_hash(res, password):
+                    self.w = Main_Window()
+                    self.w.show()
+                    self.close()
+                else:
+                    msg = QMessageBox()
+                    msg.setWindowTitle("Ошибка")
+                    msg.setText("Неправильный пароль")
+                    msg.setIcon(QMessageBox.Warning)
+                    msg.exec_()
+                cur.close()
+                con.close()
             except requests.exceptions.RequestException:
                 msg = QMessageBox()
                 msg.setWindowTitle("Ошибка")
@@ -39,22 +71,6 @@ class Autorize_Form(QDialog):
                 msg.setIcon(QMessageBox.Warning)
                 msg.exec_()
                 return None
-        if login and password:
-            con = sqlite3.connect('data/datebase/application.db')
-            cur = con.cursor()
-            com = 'SELECT password FROM user WHERE login = "' + login + '"'
-            res = cur.execute(com).fetchone()[0]
-            con.close()
-            if res == password:
-                self.w = Main_Window()
-                self.w.show()
-                self.close()
-            else:
-                msg = QMessageBox()
-                msg.setWindowTitle("Ошибка")
-                msg.setText("Неправильный пароль")
-                msg.setIcon(QMessageBox.Warning)
-                msg.exec_()
         else:
             msg = QMessageBox()
             msg.setWindowTitle("Ошибка")
@@ -63,7 +79,16 @@ class Autorize_Form(QDialog):
             msg.exec_()
 
     def sign_up(self):
-        webbrowser.open('http://127.0.0.1:5000/registration_users')
+        try:
+            requests.post('http://127.0.0.1:5000/cheak_server')
+            webbrowser.open('http://127.0.0.1:5000/registration_users')
+        except requests.exceptions.RequestException:
+            msg = QMessageBox()
+            msg.setWindowTitle("Ошибка")
+            msg.setText("Сервер недоступен")
+            msg.setIcon(QMessageBox.Warning)
+            msg.exec_()
+            return None
 
 
 class Main_Window(QMainWindow):
