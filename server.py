0e517d16a6e712d4a7f5bf28ab8ba5b4a93d671c
daze <is4effvladis@yandex.ru>
2022-05-04 23:59:42 +0300

    fix

--- server.py
+++
@@ -1,7 +0,0 @@
-
-
-class LoginForm(FlaskForm):
-    email = EmailField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
